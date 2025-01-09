45245f669d66d349f33a1333d1e96e6f724c65f6
shuta1123 <shutay1123@yahoo.co.jp>
2025-01-09 17:49:47 +0900

    プレハブ出現後削除

--- Assets/script/count.cs
+++ Assets/script/count.cs
@@ -4,6 +4,8 @@ public class count : MonoBehaviour
 {
     public static float atai;
     public static float purasu;
+    public GameObject puratxt;
+    public Transform canvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,6 +26,7 @@ public class count : MonoBehaviour
     }
         public void OnClick()
     {
+        Instantiate(puratxt, new Vector3( 693.0f, 334.0f, 0.0f), Quaternion.identity,canvas);
         atai+=purasu;
         Debug.Log(atai);  // ログを出力
     }
--- Assets/script/purasunumtxt.cs
+++ Assets/script/purasunumtxt.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using TMPro;
+using System;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 
 public class purasunumtxt : MonoBehaviour
 {
     public TextMeshProUGUI purasunum;
     float a;
+    double ti;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ti=Time.time;
     }
 
     // Update is called once per frame
@@ -16,5 +20,8 @@ public class purasunumtxt : MonoBehaviour
     {
         a=count.purasu;
         purasunum.text="+"+a;
+        if(Time.time-1>=ti)
+        Destroy(this.gameObject);
+
     }
 }
